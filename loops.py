acf7f55557b090a1fc07c4a19750ae75f066064d
srija1903 <64838154+srija1903@users.noreply.github.com>
2020-06-08 13:46:19 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+i=0
+for n in list1:
+    if n>0:
+        print(list1[i])
+    i+=1
+
+list2=[12,14,-95,3]
+for j in list2:
+    if j>0:
+        print(list2[i])
+    i+=1
+
